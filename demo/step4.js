5e2fdae34937cc8702a22c51577764042384c0e8
aco <wwsxuan@163.com>
2018-04-12 13:00:56 +0800

    step4 demo

--- demo/step4.js
+++ demo/step4.js
@@ -49,7 +49,7 @@ let Watcher = function (object, getter, callback) {
     this.obj = object
     this.getter = getter
     this.cb = callback
-    this.dep = []
+    this.deps = []
     this.value = undefined
 
     this.get = function () {
@@ -67,7 +67,7 @@ let Watcher = function (object, getter, callback) {
     }
 
     this.addDep = function (dep) {
-        this.dep.push(dep)
+        this.deps.push(dep)
     }
 
     this.teardown = function () {
@@ -89,10 +89,32 @@ defineReactive(object, 'num2', 4)
 let watcher = new Watcher(object, function () {
     return this.num1 + this.num2
 }, function (newValue, oldValue) {
-    console.log('这是一个监听函数，函数的新值为：' + newValue)
+    console.log(`这是一个监听函数，${object.num1} + ${object.num2} = ${newValue}`)
 })
 
 object.num1 = 3
-// 这是一个监听函数，函数的新值为：7
+// 这是一个监听函数，3 + 4 = 7
 object.num2 = 10
-// 这是一个监听函数，函数的新值为：13
+// 这是一个监听函数，3 + 10 = 13
+
+let watcher2 = new Watcher(object, function () {
+    return this.num1 * this.num2
+}, function (newValue, oldValue) {
+    console.log(`这是一个监听函数，${object.num1} * ${object.num2} = ${newValue}`)
+})
+
+object.num1 = 4
+// 这是一个监听函数，4 + 10 = 14
+// 这是一个监听函数，4 * 10 = 40
+object.num2 = 11
+// 这是一个监听函数，4 + 11 = 15
+// 这是一个监听函数，4 * 11 = 44
+
+// 测试取消
+watcher2.teardown()
+
+object.num1 = 5
+// 这是一个监听函数，5 + 11 = 16
+object.num2 = 12
+// 这是一个监听函数，5 + 12 = 17
+
