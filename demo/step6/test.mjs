c3230241ffd27f84441ea766d6e5f4259eca62a8
aco <wwsxuan@163.com>
2018-04-16 14:11:22 +0800

    step6 demo

---
+++ demo/step6/test.mjs
@@ -0,0 +1,31 @@
+import Watcher from './Watcher'
+import {observe} from "./Observe"
+
+let object = {
+    num1: 1,
+    num2: 1,
+    objectTest: {
+        num3: 1
+    },
+    arrayTest: [1, 2, 3, 4, 5]
+}
+
+observe(object)
+
+let watcher = new Watcher(object, function () {
+    return this.num1 + this.num2 + this.objectTest.num3
+}, function (newValue, oldValue) {
+    console.log(`监听函数，${object.num1} + ${object.num2} + ${object.objectTest.num3} = ${newValue}`)
+})
+
+let watcher1 = new Watcher(object, function () {
+    return this.num1 + this.num2 + this.objectTest.num3
+}, function (newValue, oldValue) {
+    console.log(`监听函数，${object.num1} + ${object.num2} + ${object.objectTest.num3} = ${newValue}`)
+})
+
+object.num1 = 2
+// 监听函数，2 + 1 + 1 = 4
+object.objectTest.num3 = 2
+// 监听函数，2 + 1 + 2 = 5
+object.arrayTest.push(10)
