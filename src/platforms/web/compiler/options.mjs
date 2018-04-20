2d65555d01860d7fd57c305e4cc4bbb6409fe3c1
aco <wwsxuan@163.com>
2018-04-20 15:35:07 +0800

    添加 vue 源码中的 有关标签处理的内容

---
+++ src/platforms/web/compiler/options.mjs
@@ -0,0 +1,24 @@
+/* @flow */
+
+import {
+    isPreTag,
+    mustUseProp,
+    isReservedTag,
+    getTagNamespace
+} from '../util/index'
+
+import {genStaticKeys} from '../../../util/normal-util'
+import {isUnaryTag, canBeLeftOpenTag} from './util'
+
+export const baseOptions = {
+    expectHTML: true,
+    modules:[],
+    directives: {},
+    isPreTag,
+    isUnaryTag,
+    mustUseProp,
+    canBeLeftOpenTag,
+    isReservedTag,
+    getTagNamespace,
+    staticKeys: genStaticKeys(modules)
+}
---
+++ test/test.mjs
@@ -0,0 +1,3 @@
+import {parse} from '../src/compiler/parser/index'
+
+parse('')
