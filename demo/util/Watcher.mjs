ae113e5f3790340feeec532b9d42551e6ec11596
aco <wwsxuan@163.com>
2018-04-27 14:27:38 +0800

    computed 机制添加

--- demo/util/Watcher.mjs
+++ demo/util/Watcher.mjs
@@ -4,13 +4,19 @@ let uid = 0
 
 export default class Watcher {
 
-    constructor(object, getter, callback) {
+    constructor(object, getter, callback, options) {
         this.id = ++uid
         this.obj = object
         this.getter = getter
         this.cb = callback
         this.deps = []
         this.value = this.get()
+        if (options) {
+            this.lazy = !!options.lazy
+        } else {
+            this.lazy = false
+        }
+        this.dirty = this.lazy
     }
 
     get() {
@@ -21,10 +27,22 @@ export default class Watcher {
     }
 
     update() {
+        if (this.lazy) {
+            this.dirty = true
+            return
+        }
         const value = this.getter.call(this.obj)
         const oldValue = this.value
         this.value = value
         this.cb.call(this.obj, value, oldValue)
+    }
+
+    /**
+     * 脏检查机制手动触发更新函数
+     */
+    evaluate() {
+        this.value = this.getter.call(this.obj)
+        this.dirty = false
     }
 
     addDep(dep) {
